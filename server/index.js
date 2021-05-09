63f301d5e2562cab9f91a08ef184d795b650508b
Magdalena Maksic <megicar998@gmail.com>
2021-05-09 12:36:01 +0200

    Izmene u UI dizajnu

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
-	'mongodb+srv://veljko:veljko@cluster0.d6eg0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+	'mongodb+srv://megi:megi@cluster0.fvatt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
