9b557162cbf5055296de17ef56291cedd5c717c4
Vanja <vanjolinav@gmail.com>
2021-05-10 19:46:48 +0200

    Korekcija pocetne stranice

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
-	'mongodb+srv://megi:megi@cluster0.fvatt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+	'mongodb+srv://vanja:vanja@cluster0.0skrk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
